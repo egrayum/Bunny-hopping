eddcceb62ee517467141c52a74c71fb958e9c4b2
Ethan <48872919+egrayum@users.noreply.github.com>
2021-01-10 08:39:05 -0700

    started nunjucks templating test

---
+++ project/gulpfile.js
@@ -0,0 +1,10 @@
+gulp.task('nunjucks', function() {
+  // Gets .html and .nunjucks files in pages
+  return gulp.src('app/pages/**/*.+(html|nunjucks)')
+  // Renders template with nunjucks
+  .pipe(nunjucksRender({
+      path: ['app/templates']
+    }))
+  // output files in app folder
+  .pipe(gulp.dest('app'))
+});
